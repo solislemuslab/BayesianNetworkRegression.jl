d056ff59355f73f27e5e6ff0256964b9d5138bef
Sam Ozminkowski <samoz@umich.edu>
2021-11-27 17:43:40 -0500

    add required dependencies

--- src/BayesianNetworkRegression.jl
+++ src/BayesianNetworkRegression.jl
@@ -1,10 +1,11 @@
 module BayesianNetworkRegression
     using Random, DataFrames, LinearAlgebra, StatsBase, InvertedIndices, ProgressMeter, Distributions
-    using StaticArrays, TypedTables, GaussianDistributions, MCMCChains, Distributed
+    using StaticArrays, TypedTables, GaussianDistributions, MCMCDiagnosticTools, Distributed, Statistics
     using Base.Threads
 
     include("gig.jl")
     include("utils.jl")
+    include("convergence.jl")
     include("gibbs.jl")
 
     export GenerateSamples!
