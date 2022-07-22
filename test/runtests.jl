ade2d9f3f9cb21593d6690b88b564172ab589799
Sam Ozminkowski <samoz@umich.edu>
2022-07-22 16:33:51 -0400

    version info and instructions

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,3 +1,12 @@
+println(versioninfo())
+@static if VERSION ≥ v"1.7.0-DEV.620"
+    println(BLAS.get_config())
+else
+    @show BLAS.vendor()
+    if startswith(string(BLAS.vendor()), "openblas")
+        println(BLAS.openblas_get_config())
+    end
+end
 
 using BayesianNetworkRegression,Test,LinearAlgebra,Distributions
 using CSV,DataFrames,StaticArrays,TypedTables,Random,Distributed
