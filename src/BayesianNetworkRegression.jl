fe8709f26a70a1592d63c5744cee7b0313dca883
Sam Ozminkowski <samoz@umich.edu>
2022-07-22 12:56:19 -0400

    add MKL

--- src/BayesianNetworkRegression.jl
+++ src/BayesianNetworkRegression.jl
@@ -1,7 +1,7 @@
 module BayesianNetworkRegression
     using Random, DataFrames, LinearAlgebra, StatsBase, InvertedIndices, ProgressMeter, Distributions
     using StaticArrays, TypedTables, GaussianDistributions, MCMCDiagnosticTools, Distributed, Statistics
-    using Base.Threads,KahanSummation
+    using Base.Threads,KahanSummation,MKL
 
     include("gig.jl")
     include("utils.jl")
