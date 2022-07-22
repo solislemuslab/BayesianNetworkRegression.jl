1a20cb1cb4bba2704b52d665bcceb06164d20d32
Sam Ozminkowski <samoz@umich.edu>
2022-07-22 16:27:30 -0400

    add test for worker vs master

--- test/runtests.jl
+++ test/runtests.jl
@@ -140,6 +140,43 @@ seed = 2358
     ci_df[:,"0.025"] = γ_sorted[lw,:,1]
     ci_df[:,"0.975"] = γ_sorted[hi,:,1]
 
+    addprocs(1, exeflags="--optimize=0")
+    @everywhere begin
+        R  = 7
+        V = 30
+        nburn = 30000
+        nsamp = 20000
+        total = nburn+nsamp
+        q = floor(Int,V*(V-1)/2)
+        seed = 2358
+        num_chains = 1
+
+        data_in = DataFrame(CSV.File(joinpath(@__DIR__, "data", "mu=0.8_n_microbes=15_out=XYs_pi=0.8_samplesize=100_simnum=1.csv")))
+        edges_res = DataFrame(CSV.File(joinpath(@__DIR__,"data","R=7_mu=0.8_n_microbes=15_nu=10_out=edges_pi=0.8_samplesize=100_simnum=1.csv")))
+        nodes_res = DataFrame(CSV.File(joinpath(@__DIR__,"data","R=7_mu=0.8_n_microbes=15_nu=10_out=nodes_pi=0.8_samplesize=100_simnum=1.csv")))
+
+        X = Matrix(data_in[:,names(data_in,Not("y"))])
+        y = SVector{size(X,1)}(data_in[:,:y])
+
+    end
+    result22 = Fit!(X, y, R, η=η, V=V, nburn=nburn,nsamples=nsamp, aΔ=aΔ, 
+                    bΔ=bΔ,ν=ν,ι=ι,ζ=ζ,x_transform=false,purge_burn=10000,
+                    num_chains=num_chains,seed=seed)
+
+    @show seed 
+    nburn=10000
+    total=nburn+nsamp
+
+    γ_sorted2 = sort(result22.state.γ[nburn+1:total,:,:],dims=1)
+    @show size(γ_sorted2)
+    lw = convert(Int64, round(nsamp * 0.025))
+    hi = convert(Int64, round(nsamp * 0.975))
+    
+    ci_df2 = DataFrame(mean=mean(result22.state.γ[nburn+1:total,:,:],dims=1)[1,:])
+    ci_df2[:,"0.025"] = γ_sorted2[lw,:,1]
+    ci_df2[:,"0.975"] = γ_sorted2[hi,:,1]
+
+    @test isapprox(mean(result2.state.γ[nburn+1:total,:,:],dims=1)[1,:],mean(result22.state.γ[nburn+1:total,:,:],dims=1)[1,:])
 
     @test isapprox(mean(result2.state.γ[nburn+1:total,:,:],dims=1)[1,:], edges_res.mean,rtol=0.2)
     @test isapprox(ci_df[:,"0.025"], edges_res[:,"0.025"],rtol=0.2)
@@ -148,9 +185,11 @@ seed = 2358
     xis = zeros(30)
     for i=1:30 xis[i] = isapprox(mean(result2.state.ξ[nburn+1:total,:,:],dims=1)[1,i],nodes_res[i,"Xi posterior"],atol=0.1) end
     @test xis == ones(30)
+
+    @show DataFrame(loc = mean(result2.state.ξ[nburn+1:total,:,:],dims=1)[1,:], real = nodes_res[:,"Xi posterior"])
 end 
 
-addprocs(1,exeflags="--optimize=0")
+#addprocs(1,exeflags="--optimize=0")
 
 @testset "Result tests - worker" begin
     seed = 2358
