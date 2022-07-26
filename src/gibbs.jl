e8568098ed00fce26dce9e9376f9aa7c7e18a26a
Claudia Solis-Lemus <solislemus@wisc.edu>
2022-07-26 08:12:07 -0500

    removed V as argument in fit

--- src/gibbs.jl
+++ src/gibbs.jl
@@ -632,7 +632,7 @@ Road map of fit!:
 
 """
 function Fit!(X::AbstractArray{T}, y::AbstractVector{U}, R; η=1.01,ζ=1.0,ι=1.0,aΔ=1.0,bΔ=1.0, 
-    ν=10, nburn=30000, nsamples=20000, V=0, x_transform=true, suppress_timer=false, 
+    ν=10, nburn=30000, nsamples=20000, x_transform=true, suppress_timer=false, 
     num_chains=2, seed=nothing, purge_burn=nothing, filename="parameters.log") where {T,U}
     ## Saving parameters to file:
     logfile = open(filename,"w")
